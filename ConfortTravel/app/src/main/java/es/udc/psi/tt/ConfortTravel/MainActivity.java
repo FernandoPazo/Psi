0c9fe242126509362d5fc7cad63239b422e0a974
Fer <fernando.pazo.gonzalez@udc.es>
2025-04-09 19:23:14 +0200

    [ADD] Añadido switch para activar inerciales (pruebas)

--- ConfortTravel/app/src/main/java/es/udc/psi/tt/ConfortTravel/MainActivity.java
+++ ConfortTravel/app/src/main/java/es/udc/psi/tt/ConfortTravel/MainActivity.java
@@ -1,10 +1,18 @@
 package es.udc.psi.tt.ConfortTravel;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -14,6 +22,9 @@ import es.udc.psi.tt.ConfortTravel.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private SensorEventListener sensorListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +37,36 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        setUI();
+    }
+
+    public void setUI(){
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                float x = event.values[0];
+                float y = event.values[1];
+                float z = event.values[2];
+
+                String valores = "Acelerómetro:\nX = " + x + "\nY = " + y + "\nZ = " + z;
+                binding.textView.setText(valores);
+
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            }
+        };
+        binding.swInerciales.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                sensorManager.registerListener(sensorListener, accelerometer, SensorManager.SENSOR_DELAY_UI);
+            } else {
+                sensorManager.unregisterListener(sensorListener);
+                binding.textView.setText("Lectura detenida");
+            }
         });
     }
 }
